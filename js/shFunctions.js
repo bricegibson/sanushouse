6ffeff6bec1396fef103f26bc87a7f8d63907952
Brice <brice@bricegibson.com>
2021-04-01 00:51:47 -0600

    20210401 major changes and updates to look of site and logo

--- js/shFunctions.js
+++ js/shFunctions.js
@@ -26,7 +26,31 @@ function makeHeader(title) {
 }
 
 function topNav() {
-    
+    //Navigation (Stays on Top)
+    console.log("here");
+    const nav = document.querySelector('#nav');
+    nav.classList.add("w3-top", "w3-bar", "w3-black", "w3-padding");
+    const navTitle = document.createElement("span");
+    navTitle.innerHTML = "Welcome to Sanus House";
+    navTitle.style.fontSize = "1.4em";
+    const navLinks= document.createElement("div");
+    navLinks.classList.add("w3-right", "w3-hide-small");
+
+    let navLink = document.createElement("a");
+    navLink.href = "index.html";
+    navLink.classList.add("w3-bar-item", "w3-button");
+    navLink.innerHTML = "Home";
+    navLinks.appendChild(navLink);
+
+    navLink = document.createElement("a");
+    navLink.href = "apply.html";
+    navLink.classList.add("w3-bar-item", "w3-button");
+    navLink.innerHTML = "Apply for Residency";
+    navLinks.appendChild(navLink);
+
+
+    nav.appendChild(navTitle);
+    nav.appendChild(navLinks);
 
 }
 
@@ -53,7 +77,7 @@ function sendEmail() {
         url: `https://uk7d698mc3.execute-api.us-west-1.amazonaws.com/Dev/apply`,
         data: JSON.stringify(formJSON),
         success: function (res) {
-            $('#btnSubmitSpinner').hide();
+            //$('#btnSubmitSpinner').hide();
             $('#btnSubmitText').text("Submit");
             $('#btnSubmit').addClass("invisible");
             $('#btnSubmitStatus').addClass("alert alert-success");
@@ -67,4 +91,50 @@ function sendEmail() {
             $('#btnSubmitStatus').text(`There has been an error please call us at (385) 350-3013.`); 
         }
     });
+}
+
+function sendEmailNoJS() {
+
+    var formJSON = {};
+
+    document.querySelector('#btnSubmitText').innerHTML = "Updating..."
+
+    inputs = document.querySelectorAll("input");
+    for (i = 0; i < inputs.length; i++) {
+        formJSON[inputs[i].id] = inputs[i].value;
+    }
+
+    textarea = document.querySelectorAll("textarea");
+    for (i = 0; i < textarea.length; i++) {
+        formJSON[textarea[i].id] = textarea[i].value;
+    }
+
+    console.log(formJSON);
+
+    // $("select").each(function() {
+    //     formJSON[$(this).attr("id")] = $(this).val();
+    //     if($(this).attr("data-text")) {
+    //         formJSON[$(this).attr("data-text")] = $("option:selected", this).text();
+    //     }
+    // });
+
+    // $.ajax({
+    //     method: "POST",
+    //     url: `https://uk7d698mc3.execute-api.us-west-1.amazonaws.com/Dev/apply`,
+    //     data: JSON.stringify(formJSON),
+    //     success: function (res) {
+    //         //$('#btnSubmitSpinner').hide();
+    //         $('#btnSubmitText').text("Submit");
+    //         $('#btnSubmit').addClass("invisible");
+    //         $('#btnSubmitStatus').addClass("alert alert-success");
+    //         $('#btnSubmitStatus').text(`Thank you for your submission we will contact you soon to follow up.`);            
+    //     },
+    //     error: function(jqXHR, textStatus, errorThrown){
+    //         $('#btnSubmitSpinner').hide();
+    //         $('#btnSubmitText').text("Submit");
+    //         $('#btnSubmit').addClass("invisible");
+    //         $('#btnSubmitStatus').addClass("alert alert-success");
+    //         $('#btnSubmitStatus').text(`There has been an error please call us at (385) 350-3013.`); 
+    //     }
+    // });
 }
